d48d89e6659b55025fa130a54d38ac9a904da738
jillian <jyoti.pferd@gmail.com>
2021-03-30 21:23:48 -0400

    Created new passport.js to define JWT strategies for authentication, and auth.js to initialize login credentials

---
+++ auth.js
@@ -0,0 +1,43 @@
+const jwtSecret = 'your_jwt_secret'; // must be same key used in JWTStrategy in passport.js
+
+const jwt = require('jsonwebtoken'),
+  passport = require('passport'),
+  express = require("express"),
+  app = express(),
+  bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+
+require('./passport'); //local passport file
+
+let generateJWTToken = (user) => {
+  return jwt.sign(user, jwtSecret, {
+    subject: user.Username, //username being encoded into JWTStrategy
+    expiresIn: '7d', // token will expire in 7 days
+    algorithm: 'HS256' // algorithm to "sign" aka encode values of JWT
+  });
+}
+
+/* POST login */
+module.exports = (router) => {
+  router.post('/login', (req,res) => {
+    passport.authenticate('local', {session: false}, (error, user, info) => {
+      console.log(req.body);
+      console.log(user);
+      if(error || !user) {
+        return res.status(400).json({
+          message: 'Something is not right',
+          user: user
+        });
+      }
+      req.login(user, {session: false}, (error) => {
+        if(error) {
+          res.send(error);
+        }
+        let token = generateJWTToken(user.toJSON());
+        return res.json({user,token});
+      });
+    })(req,res);
+  });
+}
