589e1b53c035aff95b82bc773615b4278626c335
jillian <jyoti.pferd@gmail.com>
2021-04-01 20:40:57 -0400

    allowed all origins for testing.

--- index.js
+++ index.js
@@ -8,18 +8,19 @@ const express = require("express"),
 const { check, validationResult } = require('express-validator');
 
 const cors = require('cors');
-Let allowedOrgins = ['http://localhost:8008', 'http://testsite.com'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1) {  // aka not in set
-        let message = 'The CORS policy for this application doesn\'t allow access from the origin ' + origin;
-        return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
+// Let allowedOrgins = [*];
+//
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1) {  // aka not in set
+//         let message = 'The CORS policy for this application doesn\'t allow access from the origin ' + origin;
+//         return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 mongoose.connect('mongodb://localhost:27017/myFlixDB',{useNewUrlParser:true, useUnifiedTopology:true});
 mongoose.set('useFindAndModify', false);
