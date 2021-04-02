b80fa94978d8e8ab9c6cdebca0f507bd26d3bfd7
jillian <jyoti.pferd@gmail.com>
2021-04-02 08:22:33 -0400

    removed allowedOrigins for task purposes

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ const express = require("express"),
 const { check, validationResult } = require('express-validator');
 
 const cors = require('cors');
-// app.use(cors());
-let allowedOrgins = ['*'];
+ app.use(cors());
+// let allowedOrgins = ['http://heroku.com'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1) {  // aka not in set
-        let message = 'The CORS policy for this application doesn\'t allow access from the origin ' + origin;
-        return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1) {  // aka not in set
+//         let message = 'The CORS policy for this application doesn\'t allow access from the origin ' + origin;
+//         return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 
 //mongoose.connect('mongodb://localhost:27017/myFlixDB',{useNewUrlParser:true, useUnifiedTopology:true});
