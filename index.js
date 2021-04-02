5419d42801f44235f18f675ed018acb2afec15c9
jillian <jyoti.pferd@gmail.com>
2021-04-02 08:11:04 -0400

    set allowed origins and changed mongoose connecto to environment variable

--- index.js
+++ index.js
@@ -8,21 +8,23 @@ const express = require("express"),
 const { check, validationResult } = require('express-validator');
 
 const cors = require('cors');
-app.use(cors());
-// Let allowedOrgins = [*];
-//
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1) {  // aka not in set
-//         let message = 'The CORS policy for this application doesn\'t allow access from the origin ' + origin;
-//         return callback(new Error(message), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
+// app.use(cors());
+let allowedOrgins = ['http://heroku.com'];
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB',{useNewUrlParser:true, useUnifiedTopology:true});
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1) {  // aka not in set
+        let message = 'The CORS policy for this application doesn\'t allow access from the origin ' + origin;
+        return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+
+//mongoose.connect('mongodb://localhost:27017/myFlixDB',{useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.connect(process.env.CONNECTION_URI,{useNewUrlParser:true, useUnifiedTopology:true});
 mongoose.set('useFindAndModify', false);
 
 app.use(morgan('common'));
