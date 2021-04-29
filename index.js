cad90d33b8e57ad89f2c4c73bbd3fbe2dfa13c56
jjwoolridge <jyoti.pferd@gmail.com>
2021-04-29 08:55:02 +0200

    Updating on laptop
    testing direct mongoose.connect

--- index.js
+++ index.js
@@ -24,8 +24,9 @@ app.use(cors());
 
 
 //mongoose.connect('mongodb://localhost:27017/myFlixDB',{useNewUrlParser:true, useUnifiedTopology:true});
-mongoose.connect(process.env.CONNECTION_URI,{useNewUrlParser:true, useUnifiedTopology:true});
-mongoose.set('useFindAndModify', false);
+//mongoose.connect(process.env.CONNECTION_URI,{useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.connect('mongodb+srv://JJWoolridge:Bananapip22j@jw-cf-movie-api.otomx.mongodb.net/myFlixDB?', {useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.set('FindAndModify', false);
 
 app.use(morgan('common'));
 app.use(bodyParser.json());
