11db722be69476e03b82f6690d3cae1df23cc90a
jjwoolridge <jyoti.pferd@gmail.com>
2021-04-29 12:48:38 +0200

    Testing index,js locally
    returned to mongoose localhost

--- index.js
+++ index.js
@@ -9,22 +9,9 @@ const { check, validationResult } = require('express-validator');
 
 const cors = require('cors');
 app.use(cors());
-// let allowedOrgins = ['http://heroku.com'];
 
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1) {  // aka not in set
-//         let message = 'The CORS policy for this application doesn\'t allow access from the origin ' + origin;
-//         return callback(new Error(message), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
-
-
-//mongoose.connect('mongodb://localhost:27017/myFlixDB',{useNewUrlParser:true, useUnifiedTopology:true});
-mongoose.connect(process.env.CONNECTION_URI,{useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.connect('mongodb://localhost:27017/myFlixDB',{useNewUrlParser:true, useUnifiedTopology:true});
+//mongoose.connect(process.env.CONNECTION_URI,{useNewUrlParser:true, useUnifiedTopology:true});
 //mongoose.connect('mongodb+srv://JJWoolridge:Bananapip22j@jw-cf-movie-api.otomx.mongodb.net/myFlixDB?retryWrites=true&w=majority', {useNewUrlParser:true, useUnifiedTopology:true});
 //mongoose.set('FindAndModify', false);
 
