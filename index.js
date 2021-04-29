700e37bb00a0f5f3315707ffc5c05456f047eee9
jjwoolridge <jyoti.pferd@gmail.com>
2021-04-29 12:55:13 +0200

    index.js
    testing

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const { check, validationResult } = require('express-validator');
 const cors = require('cors');
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB',{useNewUrlParser:true, useUnifiedTopology:true});
-//mongoose.connect(process.env.CONNECTION_URI,{useNewUrlParser:true, useUnifiedTopology:true});
+//mongoose.connect('mongodb://localhost:27017/myFlixDB',{useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.connect(process.env.CONNECTION_URI,{useNewUrlParser:true, useUnifiedTopology:true});
 //mongoose.connect('mongodb+srv://JJWoolridge:Bananapip22j@jw-cf-movie-api.otomx.mongodb.net/myFlixDB?retryWrites=true&w=majority', {useNewUrlParser:true, useUnifiedTopology:true});
 //mongoose.set('FindAndModify', false);
 
