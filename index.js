630e3bbfc6a423398f0e724d9121ec9e50f057c5
jjwoolridge <jyoti.pferd@gmail.com>
2021-05-01 13:52:34 +0200

    exposing users for testing
    exposing users for testing client side api

--- index.js
+++ index.js
@@ -112,7 +112,8 @@ app.get('/movies/director/:Director/', passport.authenticate('jwt', {session: fa
 });
 
 // return list of users
-app.get('/users', passport.authenticate('jwt', {session: false}), (req, res) => {
+app.get('/users', function (req, res) {
+//app.get('/users', passport.authenticate('jwt', {session: false}), (req, res) => {
   Users.find()
   .then(users => {
     res.status(201).json(users);
