746c07a507f3ce8e80ff2461bf2d6041272ec04d
jjwoolridge <jyoti.pferd@gmail.com>
2021-05-06 20:49:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ app.use('*',cors());
 // }));
 
 //mongoose.connect('mongodb://localhost:27017/myFlixDB',{useNewUrlParser:true, useUnifiedTopology:true});
-mongoose.connect(process.env.CONNECTION_URI,{useNewUrlParser:true, useFindandModify:false, useUnifiedTopology: true});
-//mongoose.set('useUnifiedTopology', true);
+mongoose.connect(process.env.CONNECTION_URI,{useNewUrlParser:true, useFindandModify:false});
+mongoose.set('useUnifiedTopology', true);
 
 app.use(morgan('common'));
 app.use(bodyParser.json());
