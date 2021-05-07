ead00ddd1e75338d250221fd3df9e9b085951f6b
jjwoolridge <jyoti.pferd@gmail.com>
2021-05-07 09:46:58 +0200

    Update models.js
    not need name for verification

--- models.js
+++ models.js
@@ -29,7 +29,7 @@ let userSchema = mongoose.Schema({
   Username: {type: String, required: true},
   Password: {type: String, required: true},
   Email: {type: String, required: true},
-  Name: {type: String, required: true},
+  Name: {type: String, required: false},
   Birthday: Date,
   FavoriteMovies: [{type:mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
